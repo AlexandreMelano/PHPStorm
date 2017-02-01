89b45aac430472b8aa958cfde36169c792279cfe
AlexandreMelano <alexandremelano@yahoo.com>
2017-02-01 18:06:16 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+// connection string
+$dsn = 'mysql:host=ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=gamedb';
+$userName = 's969adv5fd5oyr79';
+$password = 'gvwp5mzeiarwhaa6';
+//testsajgdasjgdjkas
+//asdasdasdasdasdasdasdasd
+// exception handling - use a try / catch
+echo "hello worlds";
+try {
+    // instantiates a new pdo - an db object
+    $db = new PDO($dsn, $userName, $password);
+echo "hello";
+}
+catch(PDOException $e) {
+    $message = $e->getMessage();
+    echo "An error occurred: " . $message;
+}
+
+$query = "SELECT * FROM games"; // SQL statement
+$statement = $db->prepare($query); // encapsulate the sql statement
+$statement->execute(); // run on the db server
+$games = $statement->fetchAll(); // returns an array
+$statement->closeCursor(); // close the connection
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>COMP1006</title>
+    <!-- CSS Section -->
+    <link rel="stylesheet" href="./Scripts/lib/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./Scripts/lib/bootstrap/dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="./Scripts/lib/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="./Content/app.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-3 col-md-6">
+            <h1>PHP with MySQL</h1>
+
+            <ul>
+                <?php
+                foreach($games as $game) {
+                    echo '<li>' . $game['Id'] . " " . $game['Name'] . " " . $game['Cost'] .'</li>';
+                }
+                ?>
+            </ul>
+
+        </div>
+    </div>
+</div>
+
+<!-- JavaScript Section -->
+<script src="./Scripts/lib/jquery/dist/jquery.min.js"></script>
+<script src="./Scripts/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./Scripts/app.js"></script>
+</body>
+</html>
